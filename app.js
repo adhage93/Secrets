fed70d654656abcf8cae4160fd00cf3aaface13c
Aditya Dhage <adhage93@gmail.com>
2019-09-19 11:10:25 +0530

    passport-half

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const md5 = require('md5');
 const mongoose = require('mongoose');
-//const encrypt = require('mongoose-encryption');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 
 const app = express();
 
@@ -17,6 +18,15 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
@@ -24,7 +34,14 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStratergy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.route("/")
     .get((req, res) => {
