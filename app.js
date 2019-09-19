b9a7840961181901e5e9d349507cfaba4399f217
Aditya Dhage <adhage93@gmail.com>
2019-09-19 10:51:50 +0530

    bcrypt style

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const md5 = require('md5');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+//const encrypt = require('mongoose-encryption');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -20,8 +23,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -44,13 +45,17 @@ app.route("/login")
             if (!err) {
                 if (foundUser) {
                     console.log("User found successfully!");
-                    if (foundUser.password === req.body.password) {
-                        console.log("Logged in successfully");
-                        res.render("secrets");
-                    }
-                    else {
-                        console.log("Wrong password!!!");
-                    }
+                    bcrypt.compare(md5(req.body.password), foundUser.password, (err, result) => {
+
+                        if (result === true) {
+                            console.log("Logged in successfully");
+                            res.render("secrets");
+                        }
+                        else {
+                            console.log("Wrong password!!!");
+                        }
+                    });
+
                 }
                 else {
                     console.log("Wrong Username!!!");
@@ -67,21 +72,29 @@ app.route("/register")
         res.render("register");
     })
     .post((req, res) => {
-        const newUser = new User({
-            email: req.body.username,
-            password: req.body.password
-        });
 
-        newUser.save((err) => {
+        bcrypt.hash(md5(req.body.password), saltRounds, (err, hash) => {
             if (!err) {
-                console.log("User registerd successfully!");
-                res.render("secrets");
+                const newUser = new User({
+                    email: req.body.username,
+                    password: hash
+                });
+
+                newUser.save((err) => {
+                    if (!err) {
+                        console.log("User registerd successfully!");
+                        res.render("secrets");
+                    }
+                    else {
+                        console.log("Error registering user: " + err);
+                    }
+                });
             }
             else {
-                console.log("Error registering user: " + err);
+                console.log("err in hashing: ", err);
             }
-        });
 
+        });
     });
 
 app.listen(3000, () => {
